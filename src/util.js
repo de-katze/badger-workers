77ea15873cd850053a779be14ae57cb2145f0204
Katze <git@katze.click>
2023-12-28 22:37:31 -0500

    switch to jsdelivr

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 const getLogo = async (name, logoColor) => {
     try {
-        let logo = await fetch(`https://esm.sh/simple-icons/icons/${name}.svg`)
+        let logo = await fetch(`https://cdn.jsdelivr.net/npm/simple-icons/icons/${name}.svg`)
         logo = await logo.text()
 
         return `data:image/svg+xml;base64,${btoa(logo.replace('<svg role="img"', `<svg role="img" fill="${logoColor || `#FFFFFF`}"`))}`;
